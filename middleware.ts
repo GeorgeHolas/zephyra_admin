62b8bac7e2c99993ddd6819243669d525067c737
GeorgeHolas <georgeholas404@gmail.com>
2024-07-16 09:55:23 -0600

    Fix to redirect to sign-in automatically

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,20 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-const isPublicRoute = createRouteMatcher(['/api/:path*', '/:path*']);
+const publicRoutes = ['/sign-in', '/api/trpc'];
 
-const middleware = clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
+export default clerkMiddleware((auth, request) => {
+  const { pathname } = new URL(request.url);
+  
+  if (!publicRoutes.some(route => pathname.startsWith(route))) {
+    const { userId } = auth();
+    if (!userId) {
+      const signInUrl = new URL('/sign-in', request.url);
+      signInUrl.searchParams.set('redirect_url', request.url);
+      return NextResponse.redirect(signInUrl);
+    }
   }
 });
-
-export default middleware;
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
